f40f32aab977d84abbdfb49beede9187af80d7ad
Tricia Sawyer <tricia.sawyer03@gmail>
2023-07-07 13:28:25 -0500

    add grid

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -6,7 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-// import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Grid';
 
 function Products() {
     const { activeCategory, products } = useSelector((state) => state.store)
@@ -16,9 +16,11 @@ function Products() {
             <When condition={activeCategory}>
                 <h2>{activeCategory.displayName}</h2>
                 <h4>Category Description Goes Here</h4>
+                <Grid container spacing={2} width="80%" marging="auto">
                 {
                     products.map((product, index) => (
-                        <Card key={`products${index}`} sx={{ maxWidth: 345 }}>
+                        <Grid key={`products${index}`} item xs={12} md={6} lg={4} >
+                        <Card sx={{ maxWidth: 345 }}>
                             <CardMedia
                                 sx={{ height: 140 }} image={`https://source.unsplash.com/random?${product.name}`}
                                 title={product.name} />
@@ -33,8 +35,10 @@ function Products() {
                                 <Button size="small">VIEW DETAILS</Button>
                             </CardActions>
                         </Card>
+                        </Grid>
                     ))
                 }
+                </Grid>
             </When>
         </>
     )
