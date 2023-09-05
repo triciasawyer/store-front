3ca01ac6209b5481651c2eab0699d07f378998c6
Tricia Sawyer <tricia.sawyer03@gmail>
2023-09-04 22:35:28 -0500

    get rid of comment

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Button from '@mui/material/Button';
 import { getCategories, setCategory } from '../../store/categories';
-// import { setProducts } from '../../store/products';
 
 function Categories() {
 
