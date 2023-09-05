b40457cf20cde258bd051e5a0dfbb244f0c90bc6
Tricia Sawyer <tricia.sawyer03@gmail>
2023-09-04 22:45:26 -0500

    add active category indicator to users

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -7,6 +7,8 @@ function Categories() {
 
   const categories = useSelector((state) => state.categories.categories);
   // console.log('Categories', categories);
+  const activeCategory = useSelector((state) => state.categories.activeCategory);
+  // console.log('Active Category:', activeCategory);
   const dispatch = useDispatch();
 
   const setDispatcher = (category) => {
@@ -17,17 +19,25 @@ function Categories() {
     dispatch(getCategories());
   }, []);
 
-  
+
   return (
     <>
-      <h2 style={{ marginTop: '100px', textAlign: 'center'}}>Browse our Categories</h2>
+      <h2 style={{ marginTop: '100px', textAlign: 'center' }}>Browse our Categories</h2>
       {categories.map((category, index) => (
-        <Button key={`categories-${index}`} onClick={() => setDispatcher(category)}>
+        <Button
+          key={`categories-${index}`}
+          onClick={() => setDispatcher(category)}
+          style={{
+            fontWeight: activeCategory && activeCategory.name === category.name ? 'bold' : 'normal',
+            color: activeCategory && activeCategory.name === category.name ? '#007bff' : 'inherit',
+          }}
+        >
           {category.name}
         </Button>
       ))}
     </>
   );
+
 }
 
 export default Categories;
